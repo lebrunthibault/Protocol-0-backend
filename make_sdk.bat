b09599e7be3b14e30da912353f0ab7952f92be8a
Thibault <thibaultlebrun@live.fr>
2021-06-13 23:55:18 +0200

    backup

--- make_sdk.bat
+++
@@ -1,4 +0,0 @@
-openapi-generator generate -i http://localhost:8000/openapi.json -g python-legacy -o api_client -t openapi_templates
-if exist "${EGG_FILE}" rm "${EGG_FILE}"
-cd api_client && python setup.py install --user
-cd ..
