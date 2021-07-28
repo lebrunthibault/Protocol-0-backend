05ae5729577fc5b0c005554b5bd26035a4692fa1
Thibault <thibaultlebrun@live.fr>
2021-07-28 12:50:44 +0200

    'backup'

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def main(language_file, encoding, errors):
     line = language_file.readline()
     if line:
         print_line(line, encoding, errors)
-    return main(language_file, encoding, errors)
+        return main(language_file, encoding, errors)
 
 
 def print_line(line, encoding, errors):
@@ -15,7 +15,8 @@ def print_line(line, encoding, errors):
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors=errors)
 
-    print(raw_bytes, "<===>", cooked_string)
+    if next_lang != cooked_string:
+        print(raw_bytes, "<===>", cooked_string, next_lang == cooked_string)
 
 languages = open("languages.txt", encoding="utf-8")
 main(languages, encoding, error)
