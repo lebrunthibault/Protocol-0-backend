f99e37044895d5a1f9bafde85111d65ff524e882
Thibault <thibaultlebrun@live.fr>
2021-12-16 01:23:23 +0100

    'backup'

--- scripts/powershell/open_protocol0_system.ps1
+++ scripts/powershell/open_protocol0_system.ps1
@@ -1,6 +1,6 @@
 . C:\Users\thiba\google_drive\music\dev\scripts\powershell\libs\VirtualDesktop.ps1
 
-Start-Sleep 15  # waiting for loop midi
+Start-Sleep 20  # waiting for loop midi
 
 vdesk on:4 run:C:\Users\thiba\AppData\Local\Microsoft\WindowsApps\wt.exe --maximized -p "Protocol0 System midi server"
 
