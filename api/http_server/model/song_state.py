0d7635fcb9219e9a681d1b9080174dbcc7d05842
Thibault <thibaultlebrun@live.fr>
2022-09-13 15:30:17 +0200

    'backup'

--- api/http_server/model/song_state.py
+++ api/http_server/model/song_state.py
@@ -7,5 +7,6 @@ class SongState(BaseModel):
     drum_track_names: List[str]
     drum_categories: List[str]
     favorite_device_names: List[List[str]]
+    insert_favorite_device_names: List[str]
     drum_rack_visible: bool
     room_eq_enabled: bool
