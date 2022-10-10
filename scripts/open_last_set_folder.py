e4db0d627f96f43c02860f8eaa9e1208d52e786a
Thibault <thibaultlebrun@live.fr>
2022-10-10 16:30:02 +0200

    'backup'

---
+++ scripts/open_last_set_folder.py
@@ -0,0 +1,13 @@
+import os
+from os.path import dirname
+
+from loguru import logger
+
+import make_path  # noqa
+from lib.ableton.ableton import get_last_set
+
+if __name__ == "__main__":
+    folder = dirname(get_last_set())
+    logger.info(f"Opening {folder}")
+    os.startfile(folder)
+
