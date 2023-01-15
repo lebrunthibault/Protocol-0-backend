13ffbce6ff3370d7c2edf803c0b0e29102e395f3
Thibault <thibaultlebrun@live.fr>
2023-01-15 16:43:43 +0100

    'Backup'

--- lib/ableton/matching_track/save_track.py
+++ lib/ableton/matching_track/save_track.py
@@ -19,7 +19,7 @@ from protocol0.application.command.EmitBackendEventCommand import (
 )
 
 
-@retry(50, 0)  # 5 seconds max
+@retry(100, 0)  # 10 seconds max
 def _wait_for_track_save(set: AbletonSet):
     sleep(0.1)
     assert set.is_current_track_saved, "Track not yet saved"
